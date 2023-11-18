a0704d9ca96d99453d1eca62af26492dbb3b7e66
Azat Idiatov <azatidiatov137@gmail.com>
2023-11-18 16:39:47 +0300

    first commit

---
+++ src/views/AboutPage.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "AboutPage",
+};
+</script>
+
+<template>
+  <div class="about-page">AboutPage</div>
+</template>
+
+<style lang="scss" scoped>
+.about-page {
+}
+</style>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<template>
+  <div class="home-page">HomePage</div>
+</template>
+
+<style lang="scss" scoped>
+.home-page {
+  background: $red;
+  @include h1;
+}
+</style>
